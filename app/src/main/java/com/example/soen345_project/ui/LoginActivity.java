99ae21165b6ce33bacb7c94de262fed72df67d52
simonbern <sbern678@gmail.com>
2026-03-12 16:32:37 -0400

    Added missing imports

--- app/src/main/java/com/example/soen345_project/ui/LoginActivity.java
+++ app/src/main/java/com/example/soen345_project/ui/LoginActivity.java
@@ -1,5 +1,9 @@
 package com.example.soen345_project.ui;
 
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.soen345_project.R;
+
 public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
