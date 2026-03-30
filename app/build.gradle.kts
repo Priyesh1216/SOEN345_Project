a49d559d75866c8ce41457347c212132725b615d
Priyesh Patel <priyeshpatel1216@gmail.com>
2026-03-30 19:23:54 -0400

    Added UI test for admin actions with Espresso + updated the gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,12 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 
     testImplementation("org.mockito:mockito-core:5.3.1")
+
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test:core:1.5.0")
 }
 
 tasks.withType<Test> {
